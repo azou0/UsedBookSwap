d8f8dea95f9ea53a9c1062db0bea6000dafbbe26
Qia Zou <qia.zou@connect.qut.edu.au>
2025-08-24 17:30:09 +1000

    User Story 9: Implement the functionality that allows to view swapped book lists

--- backend/routes/bookRoutes.js
+++ backend/routes/bookRoutes.js
@@ -7,7 +7,36 @@ const { getBooks,
 const { protect } = require('../middleware/authMiddleware');
 const router = express.Router();
 
+//search & browse functionality
+
+const { searchBooks, browseBooks } = require('../controllers/bookSearchController');
+
+// Auth users can search and browse books
+router.get('/search', protect, searchBooks);
+
+router.get('/browse', browseBooks);
+
 router.route('/').get(protect, getBooks).post(protect, addBook);
 router.route('/:id').put(protect, updateBook).delete(protect, deleteBook);
 
 module.exports = router;
+
+//view a user's available books
+
+const { getUserBooks } = require('../controllers/bookUserController');
+
+router.get('/user/:userId', getUserBooks);
+
+//send message
+
+const { sendMessage } = require('../controllers/messageController');
+const { protect } = require('../middleware/authMiddleware');
+
+router.post('/', protect, sendMessage);                 
+router.get('/thread', protect, getThread); 
+
+//view swapped books
+
+const { getSwappedBooks } = require('../controllers/bookSwapController');
+
+router.get('/swapped', getSwappedBooks);
